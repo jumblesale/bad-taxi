f4c3a0da0bf38c3d04326c3f4d83df495195d70d
Rob Sollars <rob.sollars@thoughtworks.com>
2023-03-08 12:57:42 +0100

    refactored to use cardinal not relative directions, adds test list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import *
+
 from hamcrest import *
 
 
@@ -6,9 +7,18 @@ def taxi(starting_position: Tuple[int, int], tokens: str) -> Tuple[int, int]:
     return 0, 1
 
 
+'''
+    Test list:
+        - [X] N -> (0, 1)
+        - [ ] E -> (1, 0)
+        - [ ] S -> (0, -1)
+        - [ ] W -> (-1, 0)
+'''
+
+
 def test_moving_up():
     # assert
-    token = 'u'
+    token = 'N'
 
     # act
     result = taxi((0, 0), token)
