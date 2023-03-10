0f99a2cb8f276afd95ec1b1d57584f8c73afb912
Rob Sollars <rob.sollars@thoughtworks.com>
2023-03-10 16:45:24 +0100

    adds the starting position to the list of locations returned from trip_planner

--- bad_taxi/trip_planner.py
+++ bad_taxi/trip_planner.py
@@ -8,6 +8,8 @@ Direction = Literal['n', 'e', 's', 'w']
 Velocity = NamedTuple('Velocity', [('speed',     int),
                                    ('direction', Direction)])
 Movement = Callable[[Coordinates, Direction], Coordinates]
+VelocityGenerator = Generator[Velocity, None, None]
+CoordinateGenerator = Generator[Coordinates, None, None]
 
 
 class BadTaxi:
@@ -20,23 +22,24 @@ class BadTaxi:
         return list(BadTaxi.generate_coordinates(starting_position, Parser.parse(tokens)))
 
     @staticmethod
-    def generate_coordinates(starting_position: Coordinates, velocities: List[Velocity]) -> Generator[Coordinates, None, None]:
+    def generate_coordinates(starting_position: Coordinates, velocities: VelocityGenerator) -> CoordinateGenerator:
         current_position = starting_position
+        yield current_position
         for velocity in velocities:
             speed = velocity.speed
             while speed > 0:
                 speed = speed - 1
-                current_position = BadTaxi.apply_direction(velocity.direction)(current_position)
+                current_position = BadTaxi.direction_to_movement(velocity.direction)(current_position)
                 yield current_position
 
     @staticmethod
-    def apply_direction(direction: Direction) -> Movement:
+    def direction_to_movement(direction: Direction) -> Movement:
         def _move(x: int, y: int) -> Movement:
             def _step(_position: Coordinates) -> Coordinates:
                 return _position[0] + x, _position[1] + y
             return _step
 
-        def _direction_to_movement(_direction: Direction) -> Movement:
+        def _direction_to_movement_map(_direction: Direction) -> Movement:
             return {
                 'n': _move(0, 1),
                 'e': _move(1, 0),
@@ -44,7 +47,7 @@ class BadTaxi:
                 'w': _move(-1, 0),
             }[_direction]
 
-        return _direction_to_movement(direction)
+        return _direction_to_movement_map(direction)
 
 
 class TestBadTaxi:
@@ -61,13 +64,13 @@ class TestBadTaxi:
         # assert
         assert_that(final_position, equal_to(expected))
 
-    @pytest.mark.parametrize("movement, expected", [
+    @pytest.mark.parametrize("directions, expected", [
         ('3n',         (0, 3)),
         ('4w2senenwn', (-3, 1)),
     ])
-    def test_moving_in_a_direction_multiple_times(self, movement: str, expected: Tuple[int, int]):
+    def test_moving_in_a_direction_multiple_times(self, directions: str, expected: Tuple[int, int]):
         # act
-        final_position = TestBadTaxi.a_taxi_starting_from_0_0(movement)
+        final_position = TestBadTaxi.a_taxi_starting_from_0_0(directions)
 
         # assert
         assert_that(final_position, equal_to(expected))
@@ -78,25 +81,27 @@ class TestBadTaxi:
 
 
 class TestPlanningATrip:
-    @pytest.mark.parametrize("starting_position, movement, expected_route", [
+    @pytest.mark.parametrize("starting_position, directions, expected_route", [
         ((1, -3), '3n2enw', [
-            (1, 0),
-            (3, 0),
+            (1, -3),
+            (1, -2), (1, -1), (1, 0),
+            (2, 0),  (3, 0),
             (3, 1),
             (2, 1),
         ]),
-        ((-8, 2), '100w2se3n', [
-            (-108, 2),
-            (-108, 0),
-            (-107, 0),
-            (-107, 3),
+        ((-8, 2), '4w2se3n', [
+            (-8, 2),
+            (-9, 2),  (-10, 2), (-11, 2), (-12, 2),
+            (-12, 1), (-12, 0),
+            (-11, 0),
+            (-11, 1), (-11, 2), (-11, 3),
         ]),
     ])
-    def test_it_provides_a_plan_of_the_trip(
-        self, starting_position: Coordinates, movement: str, expected_route: List[Coordinates]
+    def test_planning_a_trip(
+        self, starting_position: Coordinates, directions: str, expected_route: List[Coordinates]
     ):
         # act
-        route_plan = BadTaxi.plan_trip(starting_position, movement)
+        route_plan = BadTaxi.plan_trip(starting_position, directions)
 
         # assert
         assert_that(route_plan, equal_to(expected_route))
@@ -104,15 +109,12 @@ class TestPlanningATrip:
 
 class Parser:
     @staticmethod
-    def parse(tokens: str) -> List[Velocity]:
-        velocities = []
-        groups = re.findall(r'((\d+)([nswe]))+?|([nswe])', tokens.lower())
-        for _, speed, multiple_direction, single_direction in groups:
-            if speed:
-                velocities.append(Velocity(int(speed), multiple_direction))
-            elif single_direction:
-                velocities.append(Velocity(1, single_direction))
-        return velocities
+    def parse(tokens: str) -> VelocityGenerator:
+        match_iterator = re.finditer(r'((\d+)([nswe]))+?|([nswe])', tokens.lower())
+        for match in match_iterator:
+            _, speed, multiple_direction, single_direction = match.groups()
+            yield Velocity(int(speed), multiple_direction) if speed else \
+                Velocity(1, single_direction)
 
 
 class TestParse:
@@ -127,4 +129,4 @@ class TestParse:
         ('4s2ne2n', [Velocity(4, 's'), Velocity(2, 'n'), Velocity(1, 'e'), Velocity(2, 'n')]),
     ])
     def test_it_parses_directions_to_velocities(self, tokens: str, expected: List[Velocity]):
-        assert_that(Parser.parse(tokens), equal_to(expected))
+        assert_that(list(Parser.parse(tokens)), equal_to(expected))
